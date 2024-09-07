0013d0b90b3a98dcfb76ca5f0f635986b42a7e65
MIRNOMAN <you@example.comabdullahalnoman1509@gmail.com>
2024-09-08 01:24:36 +0600

    create my booking get successfully

--- src/app/modules/Service/service.router.ts
+++ src/app/modules/Service/service.router.ts
@@ -7,22 +7,25 @@ import {
   updateServiceController,
 } from './service.controller';
 import { adminMiddleware, authMiddleware } from './service.adminAuthorization';
-import { updateServiceSchema } from './service.validation';
+import { createServiceSchema, updateServiceSchema } from './service.validation';
 import { validateRequest } from '../../middlewares/validateRequest';
+import auth from '../../middlewares/auth';
 
 const router = express.Router();
 
 router.post('/', authMiddleware, adminMiddleware, createService);
-router.get('/:id', getServiceController);
-router.get('/', getAllServices);
+router.get('/:id', validateRequest(createServiceSchema), getServiceController);
+router.get('/', validateRequest(createServiceSchema), getAllServices);
 router.put(
   '/:id',
+  auth('admin'),
   validateRequest(updateServiceSchema),
   updateServiceController,
 );
 
 router.delete(
   '/:id',
+  auth('admin'),
   validateRequest(updateServiceSchema),
   deleteServiceController,
 );
--- src/app/modules/Slot/slot.route.ts
+++ src/app/modules/Slot/slot.route.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 import { SlotControllers } from './slot.controller';
+import { validateRequest } from '../../middlewares/validateRequest';
+import { serviceValidation } from './slot.validation';
 
 const router = express.Router();
 
-router.post('/', SlotControllers.createSlotController);
-router.get('/availability', SlotControllers.getSlotControllers);
+router.post(
+  '/',
+  validateRequest(serviceValidation.createSlotSchema),
+  SlotControllers.createSlotController,
+);
+router.get(
+  '/availability',
+  validateRequest(serviceValidation.getAvailableSlotsQuerySchema),
+  SlotControllers.getSlotControllers,
+);
 
 export const slotRoutes = router;
