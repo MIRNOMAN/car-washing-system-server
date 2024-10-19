a14dc8ee904a080ab0872e79e89747abb8be86a4
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-19 17:30:02 +0600

    create controller updateSpecificUser

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -114,7 +114,68 @@ const getUserForRecoverAccount = catchAsync(async (req: Request, res: Response, 
 
 
 
+const updateSpecificUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  const { email } = req.query;
 
+  // Check if the role or email is being changed
+  if (req.body.role || req.body.email) {
+      return sendResponse(res, {
+          statusCode: httpStatus.FORBIDDEN,
+          success: false,
+          message: 'Role and email cannot be changed. Contact admin if it is important.',
+          data: null,
+      });
+  }
+
+  // Validate the authorization header
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
+      return sendResponse(res, {
+          statusCode: httpStatus.UNAUTHORIZED,
+          success: false,
+          message: 'You have no access to this route.',
+          data: null,
+      });
+  }
+
+  const extractToken = authHeader.slice(7);
+
+  // Verify the JWT token
+  jwt.verify(extractToken, config.jwt_access_secret as string, async (err, decoded) => {
+      if (err) {
+          return sendResponse(res, {
+              statusCode: httpStatus.UNAUTHORIZED,
+              success: false,
+              message: 'You have no access to this route.',
+              data: null,
+          });
+      } else {
+          // Check if the decoded user matches the email being updated
+          if ((decoded as JwtPayload).user !== email) {
+              return sendResponse(res, {
+                  statusCode: httpStatus.UNAUTHORIZED,
+                  success: false,
+                  message: 'You have no access to this route.',
+              });
+          } else {
+              try {
+                  // Call the service to update the user
+                  const result = await UserServices.updateSpecificUserIntoDb(req.body, email as string, next);
+                  if (result) {
+                      return sendResponse(res, {
+                          statusCode: result.statusCode || httpStatus.OK,
+                          success: result.success,
+                          message: result.message,
+                          data: result.data,
+                      });
+                  }
+              } catch (error) {
+                  next(error);
+              }
+          }
+      }
+  });
+});
 
 
 
@@ -134,5 +195,6 @@ export const UserControllers = {
   SignInUser,
   getFullUserObj,
   recoverAccount,
-  getUserForRecoverAccount
+  getUserForRecoverAccount,
+  updateSpecificUser
 };
