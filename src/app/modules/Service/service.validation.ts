391996482666f1538a0251ce9256626a5f58d8c1
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-28 23:01:15 +0600

    update service validation

--- src/app/modules/Service/service.validation.ts
+++ src/app/modules/Service/service.validation.ts
@@ -1,28 +1,25 @@
-import { z } from 'zod';
+import { z } from "zod";
 
-export const createServiceSchema = z.object({
-  name: z.string().min(1, 'Service name is required'),
-  description: z.string().min(1, 'Service description is required'),
-  photo: z.string(),
-  price: z.number().positive('Price must be a positive number'),
-  duration: z.number().positive('Duration must be a positive number'),
-  isDeleted: z.boolean(),
-});
-
-export const updateServiceSchema = z.object({
-  name: z.string().optional(),
-  description: z.string().optional(),
-  photo: z.string(),
-  price: z.number().positive('Price must be a positive number').optional(),
-  duration: z
-    .number()
-    .positive('Duration must be a positive number')
-    .optional(),
-  isDeleted: z.boolean().optional(),
-});
-
-export const deleteServiceSchema = z.object({
-  params: z.object({
-    id: z.string().nonempty('Service ID is required'),
+const createServiceValidationSchema = z.object({
+  body: z.object({
+    name: z.string(),
+    description: z.string(),
+    price: z.number(),
+    duration: z.number(),
+    isDeleted: z.boolean(),
   }),
 });
+const updateServiceValidationSchema = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    description: z.string().optional(),
+    price: z.number().optional(),
+    duration: z.number().optional(),
+    isDeleted: z.boolean().optional(),
+  }),
+});
+
+export const ServiceValidation = {
+  createServiceValidationSchema,
+  updateServiceValidationSchema,
+};
